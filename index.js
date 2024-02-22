637ef88fe4409e325936e2ccca3a147ac19efb60
daniel117622 <dan5jul@hotmail.com>
2024-02-22 12:36:56 -0600

    added 404 route

--- index.js
+++ index.js
@@ -149,6 +149,13 @@ app.get('/stats', async (req, res) => {
       res.status(500).send({ error: 'Internal server error' });
     }
   });
+
+  app.use((req, res, next) => {
+    res.status(404).json({
+        message: 'Ohh you are lost, read the API documentation to find your way back home :)'
+    })
+
+  })
 // Start the server
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
